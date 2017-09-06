e5170924eecec2cc13efefe5e071044ab08b2085
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-06 09:18:27 -0500

    Completed MadLibs and finished another project.

--- Chapter 2/src/gameZone/MadLibs.java
+++ Chapter 2/src/gameZone/MadLibs.java
@@ -31,7 +31,7 @@ System.out.print("Enter a Plural Noun>>");
 PluralNoun = input.nextLine();
 System.out.print("Enter a number>>");
 a = input.nextInt();
-System.out.print("Enter another number>>");
+System.out.print("Enter a smaller number>>");
 b = input.nextInt();
 
 c = a - b;
@@ -39,8 +39,14 @@ c = a - b;
 
 
 
-System.out.print("The "+ color + "Dragon is the "+ wordEst +" Dragon of all. ");
-System.out.print("Whenever it gets hungry it hunts " + anAnimal + " for food.");
+System.out.println("The "+ color + " Dragon is the "+ wordEst +" Dragon of all. ");
+System.out.println("Whenever it gets hungry it hunts " + anAnimal + " for food.");
+System.out.println(" If the "+ color +" Dragon is tired it will take a nap in it's "+ aNoun +". Every dragon lays about "
+		+ c + " eggs a year. ");
+System.out.println("Dragon babies are always hungry and are usually found feeding around "+ PluralNoun +", It is recommended to stay away from these spots.");
+System.out.println(" Baby dragons are not as dangerous as adult dragons because they are born without "
++ bodyPartPlural + ". Because of this many people try and tame the young dragons.");
+System.out.println(" But we have yet to see any evidence of a domestic dragon.");
 
 
 	}
