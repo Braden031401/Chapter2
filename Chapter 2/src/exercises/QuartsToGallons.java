ab25838ea48d9aa85d8d18051deb05e47c153391
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-07 09:15:16 -0500

    Gallons and quarts

---
+++ Chapter 2/src/exercises/QuartsToGallons.java
@@ -0,0 +1,24 @@
+package exercises;
+
+public class QuartsToGallons {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 final int quartsInGallons = 4;
+		 
+		 int quartsNeeded = 18;
+		 int gallonsNeeded;
+		 int extraQuarts;
+		
+		 gallonsNeeded = quartsNeeded / quartsInGallons;
+		 extraQuarts = quartsNeeded % quartsInGallons;
+		 
+		 System.out.println("You have "+ gallonsNeeded +" gallons and "+ extraQuarts +" quarts left over");
+
+		
+		
+		
+		
+	}
+
+}
