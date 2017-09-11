f278bfdfe18e60420d659e54494ac20490abd913
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-11 09:27:52 -0500

    Update Notes

--- Chapter 2/src/gameZone/RandomGuessMatch.java
+++ Chapter 2/src/gameZone/RandomGuessMatch.java
@@ -11,17 +11,17 @@ public class RandomGuessMatch {
 		
 		Scanner input = new Scanner(System.in);
 
-		System.out.print("Guess a number between 1 and 5 >>>");
+		System.out.print("Guess a number between 1 and 100 >>>");
 		guess = input.nextInt();
 
-		random = 1 + (int)(Math.random() * 5);
+		random = 1 + (int)(Math.random() * 100);
 		
 		System.out.print("The number is "+ random );	
 
 		if (random == guess) {
 	
 		System.out.print(" You guessed correctly!");
-		} else { System.out.print("You guessed incorrectly!");
+		} else { System.out.print(" You guessed incorrectly!");
 	
 		}
 	}
